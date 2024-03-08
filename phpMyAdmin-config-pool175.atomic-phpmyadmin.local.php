b8b2c179cd228bea35f8fd9b5a1b9379a984ba2f
thanhtayqt <161323180+thanhtayqt@users.noreply.github.com>
2024-03-08 22:18:09 +0700

    Add files via upload

---
+++ phpMyAdmin-config-pool175.atomic-phpmyadmin.local.php
@@ -0,0 +1,7 @@
+/* phpMyAdmin configuration snippet */
+
+/* Paste it to your config.inc.php */
+
+$cfg['lang'] = 'vi';
+$cfg['Console']['Mode'] = 'collapse';
+$cfg['Export']['file_template_server'] = 'babykidsglobal.wpcomstaging.com';
